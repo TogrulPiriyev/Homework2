ed4a989e457e48e671078afdc7bad724ed1930a2
TogrulPiriyev <togrulpiriyev7@gmail.com>
2021-10-26 10:01:31 +0400

    program.cs is homework2"

---
+++ P201/P201/Homework1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace P201
+{
+    class Homework1
+    {
+        static void Main(string[] args)
+        {
+            //1.Verilmis array - deki reqemlerinin cemi 10 - dan kicik olan ededlerin sayini tapan alqoritm
+            //int[] numbers = { 23, 45, 36, 73, 91, 236 };
+            //int Counter = 0;
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    int number = 0;
+            //    int new_number;
+
+
+            //    while (numbers[i] > 0)
+            //    {
+            //        new_number = numbers[i] % 10;
+            //        number +=  new_number;
+            //        numbers[i]/=10;
+            //    }
+            //    if (number < 10) 
+            //    { 
+            //        Counter++;
+            //    }
+            //}
+            ////Console.WriteLine(Counter);
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //2. 1-den 8000-dek ededler icerisinde cut olanlarin sayini tapan proqram
+            //int Counter = 0;
+            //for (int i =1; i<8000; i++)
+            //{
+            //    if (i%2==0)
+            //    {
+            //        Counter++;
+            //    }
+            //}
+            //Console.WriteLine(Counter);
+
+
+
+
+
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //3.Verilmis ededin en yaxin asagi kokalti deyerini tapan alqoritm(kecen ders etdiyimize uygun, misalcun eded 20 - dirse 4 qaytarmalidi, 25 - dise 5 qaytarmalidi, 16 - drisa 4 ve s.)
+            //Console.WriteLine("Kok altisini tapmag uchun reqem yazin");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int j = 0;
+
+            //while ((j - 1) * (j - 1) < a)
+            //{
+            //    if (j * j > a)
+            //    {
+            //        j--;
+            //        break;
+            //    }
+            //    else if (j*j==a)
+            //    {
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        j++;
+            //    }
+
+
+            //}
+            //Console.WriteLine(j);
+            //int[] num = { 14, 25, 65, 99, 123, 232 };
+
+            //int sum = 0;
+            //int counter = 0;
+
+
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    ;
+            //    if (num[i] % 3 == 0 || num[i] % 7 == 0) 
+            //    {
+            //        sum += num[i];
+            //        counter++;
+
+            //    }
+
+
+
+
+            //}
+            //int average = sum / counter;
+            //Console.WriteLine(average);
+            //string name = Console.ReadLine();
+
+            //string surname = Console.ReadLine();
+
+
+            //while (name.Length > 3 && name.Length < 20 && surname.Length > 3 && surname.Length < 20)
+            //{
+            //    name = Console.ReadLine();
+
+            //    surname = Console.ReadLine();
+            //    if (name.Length<3 || name.Length)
+            //    {
+            //        Console.WriteLine(name + " " + surname);
+            //        break;
+            //    }
+            //    else
+            //    {
+
+            //        Console.WriteLine("Ad ya Soyad cox uzundur yada cox kisadir");
+            //    }
+            //}
+            //string a = Console.ReadLine();
+            //char c = '1';
+            //bool textmessage = true;
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    if (a[i] == c) 
+            //    {
+            //        textmessage = false;
+            //        break;
+                    
+            //    }
+                
+            //}
+            //if (textmessage == false)
+            //{
+            //    Console.WriteLine("var");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("yoxdur");
+            //}
+
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    fullname += name[i];
+            //}
+            //fullname += " ";
+            //for (int i = 0; i < surname.Length; i++)
+            //{
+            //    fullname += surname[i];
+            //}
+            //Console.WriteLine(fullname);
+
+
+
+
+        }
+    }
+}
---
+++ P201/P201/Homework2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P201
+{
+    class Homework2
+    {
+        static void Main2(string[] args)
+        {
+            string word = Console.ReadLine();
+            char[] numbers = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+            for (int i = 0; i < word.Length; i++) {
+                for (int j = 0; j < numbers.Length; j++) 
+                {   if (word[i] == numbers[j])
+                    {
+                        Console.WriteLine("var");
+                        break;
+                    }
+
+                }
+
+            }
+
+
+
+
+        }
+    }
+}
