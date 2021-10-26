6610cf474534b4cace705bf324c3d12efaf54ff7
TogrulPiriyev <togrulpiriyev7@gmail.com>
2021-10-26 10:05:47 +0400

    refresh

---
+++ P201/P201/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace P201_homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+
+
+            //Console.WriteLine("Bura sozu yazin: ");   
+            //string word = Console.ReadLine();
+            //char[] numbers = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            //bool trueFalse = false;
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //    for (int j = 0; j < numbers.Length; j++)
+            //    {
+            //        if (word[i] == numbers[j])
+            //        {
+            //              trueFalse = true;
+            //              break;
+            //        }
+
+            //    }
+
+            //}
+            //if (trueFalse) 
+            //{
+            //    Console.WriteLine("Var");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Yoxdur");
+
+            //}
+            #endregion
+            #region Task2
+            //string word = Console.ReadLine();
+            //bool trueWord = false;
+            //for (int i = 0; i <word.Length; i++)
+            //{
+            //    if (word[i]=='a' )
+            //    {
+            //        if (i == word.Length - 1)
+            //        {
+            //            Console.WriteLine("yoxdur");
+            //            break;
+            //        }
+            //        else if (word[i + 1] == 'l')
+            //        {
+            //            trueWord = true;
+            //            break;
+            //        }
+
+            //    }
+            //}
+            //if (trueWord) 
+            //{ 
+            //    Console.WriteLine("var");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("yoxdur");
+            //}
+            #endregion
+            #region Task3
+            //int num = 2567890;
+            //int sum = 0;
+            //while (num > 0)
+            //{
+            //    sum += num % 10;
+            //    num = (num - num % 10) / 10;
+            //}
+
+            
+          
+            //Console.WriteLine(sum);
+            #endregion
+        }
+    }
+}
